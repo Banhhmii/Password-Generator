a4cbb73f81afc19a110e0414bebda64b435686ff
Banh <tommyngo156@gmail.com>
2026-04-20 00:26:00 -0400

    Added more characters to be used for generation

--- main.js
+++ main.js
@@ -2,12 +2,25 @@ const prompt = require("prompt-sync")();
 
 const pwLength = parseInt(prompt("Please enter password length: "));
 const lowerCaseLetters = "abdcefghiklmnopqrstuvwxyz";
+const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+";
 let password = "";
+
 while(pwLength != 0 && pwLength > 4) {
     let count = 0;
     for (let i = 0; i < pwLength; i++) {
-        password += lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
-        count += 1;
+        let char = Math.floor(Math.random() * 4);
+        if (char === 0) {
+            password += upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
+        } else if (char === 1) {
+            password += numbers[Math.floor(Math.random() * numbers.length)];
+        } else if (char === 2) {
+            password += symbols[Math.floor(Math.random() * symbols.length)];
+        } else {
+            password += lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
+        }
+        count++;
     }
     break;
 }
